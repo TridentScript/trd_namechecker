afc41a8a1f3c1b97f814347a427e54e5069d2329
Ihsan <122683406+rwixy@users.noreply.github.com>
2025-12-23 22:46:13 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ lua54 'yes'
 
 author 'Team Trd'
 description 'Checks if player name matches Discord nickname before allowing connection'
-version '1.0.0'
+version '1.0.1'
 shared_script 'config.lua'
 server_script 'server.lua'
+
